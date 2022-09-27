35ae6e97e1ad89d6949a6a2664bbecf09394c80b
MarceloAmarillo <marceloamarillo1@gmail.com>
2022-09-27 13:57:23 -0300

    se agrego componente skill

---
+++ ma/src/main/java/com/portfoio/ma/Repository/Rhys.java
@@ -0,0 +1,12 @@
+
+package com.portfoio.ma.Repository;
+
+import com.portfoio.ma.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
